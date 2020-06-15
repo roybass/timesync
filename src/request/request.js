0429dfa8a09203dcf359a736e7954548b84c93a1
Roy Bass <roybass@gmail.com>
2020-06-15 22:32:51 +0300

    Browser/ReactNative only

--- src/request/request.js
+++ src/request/request.js
@@ -1,7 +1,1 @@
-var isBrowser = (typeof document !== 'undefined');
-var isReactNative = (typeof navigator !== 'undefined' && navigator.product === 'ReactNative');
-
-// FIXME: how to do conditional loading this with ES6 modules?
-module.exports = (isBrowser || isReactNative) ?
-    require('./request.browser') :
-    require('./request.node');
+module.exports = require('./request.browser');
